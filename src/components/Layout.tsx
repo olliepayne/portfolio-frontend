a2910cc14813a8a8e7d57b3320b34be64bacb95e
Ollie <olliepayne@me.com>
2021-08-16 20:12:14 -0700

    Add: Stub Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,8 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
 import * as React from "react"
 import Header from "./Header/Header"
-import Footer from "./Footer"
+import Footer from "./Footer/Footer"
 
 interface LayoutProps {
   children?: React.ReactNode | React.ReactNode[]
@@ -9,10 +11,17 @@ const Layout = ({
   children
 }: LayoutProps) => {
   return (
-    <>
+    <div
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column"
+      }}
+    >
       <Header />
       {children}
-    </>
+      <Footer />
+    </div>
   )
 }
 
