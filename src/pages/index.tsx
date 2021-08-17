51009833c577923c5125e8d21bcec6f37faf1f14
Ollie <olliepayne@me.com>
2021-08-16 20:02:49 -0700

    Add: Stub index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+
+    </main>
+  )
+}
+
+export default IndexPage
