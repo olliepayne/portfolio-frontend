97918b1f669becb522bb842a1c8610a12843f76a
Ollie Payne <olliepayne@me.com>
2024-09-29 23:00:38 -0700

    Add white back to theme file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,12 +16,25 @@ const config: Config = {
       sans: ['"Open Sans", system-ui']
     },
     fontSize: {
-      body: "1rem"
+      body: "1rem",
+      heading5Desktop: "1.25rem"
     },
     colors: {
       primary: "#FFD1D2",
       bone: "#F3F1D9",
-      charcoal: "#272727"
+      charcoal: "#272727",
+      white: "white"
+    },
+    spacing: {
+      0: "0rem",
+      1: "0.5",
+      2: "0.75rem",
+      3: "1rem",
+      4: "1.25rem",
+      5: "1.5rem",
+      6: "2rem",
+      7: "2.5rem",
+      8: "3rem",  
     }
   },
   plugins: []
