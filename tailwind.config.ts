346c461757f12692a1d07853e3d315b7d3267d60
Ollie Payne <olliepayne@me.com>
2024-09-29 22:37:58 -0700

    Fix charcoal theme color Needs to be a hexcode, added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     screens: {
@@ -21,7 +21,7 @@ const config: Config = {
     colors: {
       primary: "#FFD1D2",
       bone: "#F3F1D9",
-      charcoal: "272727"
+      charcoal: "#272727"
     }
   },
   plugins: []
