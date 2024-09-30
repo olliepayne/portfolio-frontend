87c0a10265368338dcdeb628bd28b6e547c5ea63
Ollie Payne <olliepayne@me.com>
2024-09-30 13:42:01 -0700

    Create PrimaryLink component Moved link styling in Navbar.tsx into the newly created PrimaryLink.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import PrimaryLink from "./PrimaryLink"
 
 const links = [
   {
@@ -28,21 +29,11 @@ export default function Navbar() {
               {links.map(({ href, text }, index) =>
                 index < links.length - 1 ? (
                   <li className="mr-6">
-                    <Link
-                      href={href}
-                      className="relative px-2 py-1 z-20 text-white transition-all delay-200 before:bg-primary before:absolute before:-z-10 before:left-0 before:top-0 before:transition-all before:delay-200 before:w-full before:h-full before:origin-bottom before:scale-0 after:transition-all after:ease-out after:duration-300 after:origin-center after:w-full after:h-1 after:scale-x-0 after:bg-primary after:absolute after:bottom-0 after:left-0 hover:text-black hover:before:scale-100 hover:after:scale-x-100"
-                    >
-                      {text}
-                    </Link>
+                    <PrimaryLink href={href} text={text} />
                   </li>
                 ) : (
                   <li>
-                    <Link
-                      href={href}
-                      className="relative px-2 py-1 z-20 text-white transition-all delay-200 before:bg-primary before:absolute before:-z-10 before:left-0 before:top-0 before:transition-all before:delay-200 before:w-full before:h-full before:origin-bottom before:scale-0 after:transition-all after:ease-out after:duration-300 after:origin-center after:w-full after:h-1 after:scale-x-0 after:bg-primary after:absolute after:bottom-0 after:left-0 hover:text-black hover:before:scale-100 hover:after:scale-x-100"
-                    >
-                      {text}
-                    </Link>
+                    <PrimaryLink href={href} text={text} />
                   </li>
                 )
               )}
---
+++ app/components/PrimaryLink.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+
+type Props = {
+  href: string
+  text: string
+}
+
+export default function PrimaryLink({ href, text }: Props) {
+  return (
+    <Link
+      href={href}
+      className="relative px-2 py-1 z-20 text-white transition-all delay-200 before:bg-primary before:absolute before:-z-10 before:left-0 before:top-0 before:transition-all before:delay-200 before:w-full before:h-full before:origin-bottom before:scale-0 after:transition-all after:ease-out after:duration-300 after:origin-center after:w-full after:h-1 after:scale-x-0 after:bg-primary after:absolute after:bottom-0 after:left-0 hover:text-black hover:before:scale-100 hover:after:scale-x-100"
+    >
+      {text}
+    </Link>
+  )
+}
