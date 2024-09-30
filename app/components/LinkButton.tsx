935e6806b0e5cf41d2a49bca5e70106701326ce3
Ollie Payne <olliepayne@me.com>
2024-09-30 14:46:09 -0700

    Create LinkButton.tsx

---
+++ app/components/LinkButton.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+type Props = {
+  href: string
+  text: string
+}
+
+export default function LinkButton({ href, text }: Props) {
+  return (
+    <Link href={href} className="px-3 py-1 border-2 border-primary transition-all hover:border-white hover:text-black hover:bg-white">
+      {text}
+    </Link>
+  )
+}
