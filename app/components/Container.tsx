8128e55b709ff2b7312ab3fcd0d56b7f28606f97
Ollie Payne <olliepayne@me.com>
2024-10-01 13:45:15 -0700

    Add narrow variant

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -2,16 +2,22 @@ import React from "react"
 
 interface Props {
   children?: React.ReactNode
-  variant?: "narrow" | "normal" | "wide"
-  className?: string
+  variant?: "narrow" | "normal"
 }
 
-export default function Container({ children, variant, className }: Props) {
-  // const chosenVariant = variant === undefined ? "normal" : variant
-
-  return (
-    <div className="max-w-screen-xl px-4 mx-auto h-full">
-      {children}
-    </div>
-  )
+export default function Container({ children, variant }: Props) {
+  switch (variant) {
+    case "narrow":
+      return (
+        <div className="max-w-screen-md px-4 mx-auto h-full">{children}</div>
+      )
+    case "normal":
+      return (
+        <div className="max-w-screen-xl px-4 mx-auto h-full">{children}</div>
+      )
+    default:
+      return (
+        <div className="max-w-screen-xl px-4 mx-auto h-full">{children}</div>
+      )
+  }
 }
