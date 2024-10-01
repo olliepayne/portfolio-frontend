8dc7ce8769759235797b7de6bb36e105095ab08a
Ollie Payne <olliepayne@me.com>
2024-09-30 17:55:49 -0700

    Begin working on homepage intro section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
+import Container from "./components/Container"
+import Image from "next/image"
+import LinkButton from "./components/LinkButton"
+import Divider from "./components/Divider"
+
 export default function Home() {
   return (
-    <section>
-      <h1 className="text-indigo-700">Hello World</h1>
-    </section>
+    <main>
+      <section className="py-16">
+        <Container>
+          <div className="flex flex-row justify-between">
+            <div className="mr-4 basis-1/2">
+              <h1 className="text-heading1Desktop font-bold">I'm Ollie</h1>
+              <p className="my-4">
+                I am a Frontend developer, SEO Analyst, and avid sport climber
+                living in Phoenix, AZ.
+              </p>
+              <div></div>
+              <div>
+                <LinkButton href="#" text="Contact Me" className="mr-4" />
+                <p className="inline-block">My projects</p>
+              </div>
+            </div>
+            <div className="ml-4 relative flex-initial">
+              <Image src="/headshot.jpeg" alt="" fill objectFit="cover" />
+            </div>
+          </div>
+        </Container>
+      </section>
+      <Divider />
+    </main>
   )
 }
