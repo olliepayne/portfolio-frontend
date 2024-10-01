a510856a1ad24226d9f4834a65c9b65d6fc0b811
Ollie Payne <olliepayne@me.com>
2024-10-01 13:49:46 -0700

    Format

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -8,14 +8,34 @@ interface Props {
 export default function Heading({ level, children }: Props) {
   switch (level) {
     case "h1":
-      return <h1 className="font-bold text-heading1Mobile md:text-heading1Desktop">{children}</h1>
+      return (
+        <h1 className="font-bold text-heading1Mobile md:text-heading1Desktop">
+          {children}
+        </h1>
+      )
     case "h2":
-      return <h2 className="font-bold text-heading2Mobile md:text-heading2Desktop">{children}</h2>
+      return (
+        <h2 className="font-bold text-heading2Mobile md:text-heading2Desktop">
+          {children}
+        </h2>
+      )
     case "h3":
-      return <h3 className="font-bold text-heading3Mobile md:text-heading3Desktop">{children}</h3>
+      return (
+        <h3 className="font-bold text-heading3Mobile md:text-heading3Desktop">
+          {children}
+        </h3>
+      )
     case "h4":
-      return <h4 className="font-bold text-heading4Mobile md:text-heading4Desktop">{children}</h4>
+      return (
+        <h4 className="font-bold text-heading4Mobile md:text-heading4Desktop">
+          {children}
+        </h4>
+      )
     case "h5":
-      return <h5 className="font-bold text-heading5Mobile md:text-heading5Desktop">{children}</h5>
+      return (
+        <h5 className="font-bold text-heading5Mobile md:text-heading5Desktop">
+          {children}
+        </h5>
+      )
   }
 }
