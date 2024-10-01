6bd39ecd36dc96c7942758957b64f0899fdfc302
Ollie Payne <olliepayne@me.com>
2024-09-30 17:34:36 -0700

    Change display to inline-block Fixes incorrect sizing and overlapping

--- app/components/LinkButton.tsx
+++ app/components/LinkButton.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function LinkButton({ href, text }: Props) {
   return (
-    <Link href={href} className="px-3 py-1 border-2 border-primary transition-all hover:border-white hover:text-black hover:bg-white">
+    <Link href={href} className="inline-block px-3 py-1 border-2 border-primary transition-all hover:border-white hover:text-black hover:bg-white">
       {text}
     </Link>
   )
