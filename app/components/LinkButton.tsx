6a473584011a0f69203ac6cf965a2a588402eb3d
Ollie Payne <olliepayne@me.com>
2024-09-30 17:52:20 -0700

    Add the option to pass custom prop styles to the LinkButton This will primarily be useful for positioning in certain use cases

--- app/components/LinkButton.tsx
+++ app/components/LinkButton.tsx
@@ -3,11 +3,15 @@ import Link from "next/link"
 interface Props {
   href: string
   text: string
+  className?: string
 }
 
-export default function LinkButton({ href, text }: Props) {
+const styles =
+  "inline-block px-3 py-1 border-2 border-primary transition-all hover:border-white hover:text-black hover:bg-primary"
+
+export default function LinkButton({ href, text, className }: Props) {
   return (
-    <Link href={href} className="inline-block px-3 py-1 border-2 border-primary transition-all hover:border-white hover:text-black hover:bg-primary">
+    <Link href={href} className={className ? `${className} ${styles}` : styles}>
       {text}
     </Link>
   )
