bf9542b75648c93573ed16b4dc9a4f9659178ff3
Ollie Payne <olliepayne@me.com>
2024-09-30 17:45:27 -0700

    Change bg color on hover to primary from white

--- app/components/LinkButton.tsx
+++ app/components/LinkButton.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link"
 
-type Props = {
+interface Props {
   href: string
   text: string
 }
 
 export default function LinkButton({ href, text }: Props) {
   return (
-    <Link href={href} className="inline-block px-3 py-1 border-2 border-primary transition-all hover:border-white hover:text-black hover:bg-white">
+    <Link href={href} className="inline-block px-3 py-1 border-2 border-primary transition-all hover:border-white hover:text-black hover:bg-primary">
       {text}
     </Link>
   )
