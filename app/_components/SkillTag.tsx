ba2b52b6fbe7e6899bcd9895ab1147fc50987f0a
Oliver Payne <olliepayne@me.com>
2024-10-05 17:33:04 -0700

    Refactor and add href pop

--- app/_components/SkillTag.tsx
+++ app/_components/SkillTag.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link"
+import { Skill } from "@/app/types"
 
-interface Props {
+interface Props extends Skill {
+  href: string
   name: string
 }
 
-export default function SkillTag({ name }: Props) {
+export default function SkillTag({ href, name }: Props) {
   return (
     <Link
-      href="#"
-      className="text-sm inline-block px-2 py-0.5 border-2 border-black transition-all  hover:text-white hover:bg-black"
+      href={href}
+      className="text-sm inline-block px-1 py-0.5 border-2 border-black transition-all  hover:text-white hover:bg-black"
     >
       {name}
     </Link>
