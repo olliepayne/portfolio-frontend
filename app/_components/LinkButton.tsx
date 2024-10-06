0f183846215aab6de32efd02aaac27a837a0ce9a
Oliver Payne <olliepayne@me.com>
2024-10-06 00:00:38 -0700

    Move primary link design in here and add slight redesign

--- app/_components/LinkButton.tsx
+++ app/_components/LinkButton.tsx
@@ -10,7 +10,7 @@ export default function LinkButton({ href, text, className }: Props) {
   return (
     <Link
       href={href}
-      className={`inline-block px-3 py-1 border-2 border-primary transition-all hover:border-white hover:text-black hover:bg-primary ${className}`}
+      className={`inline-block relative px-2 py-1 z-20 border-primary border-solid border-2 transition-all before:bg-primary before:absolute before:-z-10 before:left-0 before:top-0 before:transition-all before:delay-200 before:w-full before:h-full before:origin-bottom-left before:scale-0 after:transition-all after:ease-out after:duration-300 after:origin-center after:w-full after:h-1 after:scale-x-0 after:bg-primary after:absolute after:bottom-0 after:left-0 hover:text-black hover:before:scale-100 hover:after:scale-x-100 hover:border-transparent ${className}`}
     >
       {text}
     </Link>
