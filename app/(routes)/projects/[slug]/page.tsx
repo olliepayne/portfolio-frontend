4c2894d1acbd51fe1150fe9afee5dfbcd73b16c3
Oliver Payne <olliepayne@me.com>
2024-10-06 16:18:32 -0700

    Create and scaffold project slug page

--- app/(routes)/projects/[slug]/page.tsx
+++ app/(routes)/projects/[slug]/page.tsx
@@ -1,7 +1,27 @@
-export default function ProjectSlugPage() {
+import { getStrapiMedia } from "@/app/helpers/getStrapiMedia"
+import { Project } from "@/app/types"
+import getStrapiData from "@/app/helpers/getStrapiData"
+
+export async function generateStaticParams() {
+  const projects: Project[] = await getStrapiData("/api/projects", "no-cache")
+
+  return projects.map((project) => ({ slug: project.slug }))
+}
+
+interface Props {
+  params: {
+    slug: string
+  }
+}
+
+export default async function ProjectSlugPage({ params }: Props) {
+  const [project]: Project[] = await getStrapiData(
+    `/api/projects?filters[slug][$eq]=${params.slug}`
+  )
+
   return (
     <main>
-      <h1></h1>
+      <h1>{project.name}</h1>
     </main>
   )
 }
