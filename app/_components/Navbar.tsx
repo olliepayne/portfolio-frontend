ff399f3cc4a5959d0f2e091e73d99b0d252f9126
Oliver Payne <olliepayne@me.com>
2024-10-06 16:15:07 -0700

    Begin adding menu toggling for mobile

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,23 +1,31 @@
+"use client"
+
 import Link from "next/link"
 import LinkButton from "@/app/_components/LinkButton"
 import Container from "@/app/_components/Container"
+import { useState } from "react"
 
 const links = [
   {
-    href: "#about",
+    href: "/#about",
     text: "About"
   },
   {
-    href: "#experience",
+    href: "/#experience",
     text: "Experience"
   },
   {
-    href: "#",
+    href: "/#projects",
     text: "Projects"
   }
 ]
 
 export default function Navbar() {
+  const [navIsOpen, setNavIsOpen] = useState(false)
+  function toggleNav() {
+    setNavIsOpen(!navIsOpen)
+  }
+
   return (
     <header className="bg-charcoal text-white sticky top-0 z-50 h-16 lg:py-4 lg:h-fit">
       <Container>
@@ -28,20 +36,32 @@ export default function Navbar() {
           >
             Ollie Payne
           </Link>
-          <div className="bg-charcoal absolute left-0 top-0 max-h-screen flex justify-between pl-2 pt-16 pb-8 w-full lg:h-fit lg:p-0 lg:static lg:flex-row lg:bg-transparent lg:w-fit">
+          <div className={`bg-charcoal border-solid border-t-2 border-primary absolute transition-all left-0 top-16 overflow-hidden ${navIsOpen ? "max-h-screen" : "max-h-0"} flex justify-between pl-4 pt-16 pb-8 w-full lg:overflow-auto lg:border-none lg:h-fit lg:p-0 lg:static lg:flex-row lg:bg-transparent lg:w-fit`}>
             <ul className="flex flex-col lg:items-center lg:flex-row">
               {links.map(({ href, text }, index) => (
                 <li key={`nav-link-${index}`} className="mb-4 lg:mb-0 lg:mr-6">
-                  <Link href={href} className="inline-block hover:opacity-75 transition-opacity">
+                  <Link
+                    href={href}
+                    className="inline-block hover:opacity-75 transition-opacity"
+                  >
                     {text}
                   </Link>
                 </li>
               ))}
-              <li className="ml-2 lg:m-0">
-                <LinkButton href="#" text="Contact Me" />
+              <li>
+                <LinkButton
+                  href="#"
+                  text="Contact Me"
+                  className="hover:text-black"
+                />
               </li>
             </ul>
           </div>
+          <button className="w-8 h-5 flex flex-col justify-between lg:hidden" onClick={toggleNav}>
+            <span className="rounded-full inline-block h-0.5 w-full bg-white" />
+            <span className="rounded-full inline-block h-0.5 w-full bg-white" />
+            <span className="rounded-full inline-block h-0.5 w-full bg-white" />
+          </button>
         </nav>
       </Container>
     </header>
