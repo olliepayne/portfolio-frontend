d99993811e09560442bc4ff2b2db89c5ea444ec3
Oliver Payne <olliepayne@me.com>
2024-10-05 23:50:03 -0700

    Refactor

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link"
-import PrimaryLink from "./PrimaryLink"
-import LinkButton from "./LinkButton"
-import Container from "./Container"
+import LinkButton from "@/app/_components/LinkButton"
+import Container from "@/app/_components/Container"
 
 const links = [
   {
@@ -20,7 +19,7 @@ const links = [
 
 export default function Navbar() {
   return (
-    <header className="bg-charcoal text-white relative h-16 lg:py-4 lg:h-fit">
+    <header className="bg-charcoal text-white sticky top-0 z-50 h-16 lg:py-4 lg:h-fit">
       <Container>
         <nav className="flex flex-row justify-between items-center h-full">
           <Link
@@ -30,10 +29,12 @@ export default function Navbar() {
             Ollie Payne
           </Link>
           <div className="bg-charcoal absolute left-0 top-0 max-h-screen flex justify-between pl-2 pt-16 pb-8 w-full lg:h-fit lg:p-0 lg:static lg:flex-row lg:bg-transparent lg:w-fit">
-            <ul className="flex flex-col lg:flex-row">
+            <ul className="flex flex-col lg:items-center lg:flex-row">
               {links.map(({ href, text }, index) => (
                 <li key={`nav-link-${index}`} className="mb-4 lg:mb-0 lg:mr-6">
-                  <PrimaryLink href={href} text={text} />
+                  <Link href={href} className="inline-block hover:opacity-75 transition-opacity">
+                    {text}
+                  </Link>
                 </li>
               ))}
               <li className="ml-2 lg:m-0">
