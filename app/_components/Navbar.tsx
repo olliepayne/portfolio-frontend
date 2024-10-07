f2922e715205635be512ecc6b03909e8c9cc7aa5
Oliver Payne <olliepayne@me.com>
2024-10-06 21:24:51 -0700

    Add functional mobile nav

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -23,6 +23,10 @@ const links = [
 export default function Navbar() {
   const [navIsOpen, setNavIsOpen] = useState(false)
   function toggleNav() {
+    const newState = !navIsOpen
+    if (newState) document.body.style.overflow = "hidden"
+    else document.body.style.overflow = "auto"
+
     setNavIsOpen(!navIsOpen)
   }
 
@@ -36,7 +40,13 @@ export default function Navbar() {
           >
             Ollie Payne
           </Link>
-          <div className={`bg-charcoal border-solid border-t-2 border-primary absolute transition-all left-0 top-16 overflow-hidden ${navIsOpen ? "flex" : "none"} h-screen justify-between pl-4 pt-4 pb-8 w-full lg:flex lg:overflow-auto lg:border-none lg:h-fit lg:p-0 lg:static lg:flex-row lg:bg-transparent lg:w-fit`}>
+          <div
+            className={`bg-charcoal border-solid border-primary absolute transition-all left-0 top-16 overflow-hidden ${
+              navIsOpen
+                ? "max-h-screen border-t-2 pt-4 pb-8"
+                : "max-h-0 pt-0 pb-0"
+            } h-screen justify-between pl-4 w-full lg:max-h-fit lg:flex lg:overflow-auto lg:border-none lg:h-fit lg:p-0 lg:static lg:flex-row lg:bg-transparent lg:w-fit`}
+          >
             <ul className="flex flex-col lg:items-center lg:flex-row">
               {links.map(({ href, text }, index) => (
                 <li key={`nav-link-${index}`} className="mb-4 lg:mb-0 lg:mr-6">
@@ -57,7 +67,10 @@ export default function Navbar() {
               </li>
             </ul>
           </div>
-          <button className="w-8 h-5 flex flex-col justify-between lg:hidden" onClick={toggleNav}>
+          <button
+            className="w-8 h-5 flex flex-col justify-between lg:hidden"
+            onClick={toggleNav}
+          >
             <span className="rounded-full inline-block h-0.5 w-full bg-white" />
             <span className="rounded-full inline-block h-0.5 w-full bg-white" />
             <span className="rounded-full inline-block h-0.5 w-full bg-white" />
