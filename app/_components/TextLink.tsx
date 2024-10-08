bf6c3af7e560a3b4a66742ccfa74127eafa4511c
Oliver Payne <olliepayne@me.com>
2024-10-07 22:38:24 -0700

    Create TextLink component

---
+++ app/_components/TextLink.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+
+interface Props {
+  href: string
+  text: string
+}
+
+export default function TextLink({ href, text }: Props) {
+  return (
+    <Link
+      href={href}
+      className="relative hover:text-gray-500 transition-all after:absolute after:bottom-0.5 after:left-0 after:w-full after:h-1 after:bg-primary after:-z-10"
+    >
+      {text}
+    </Link>
+  )
+}
