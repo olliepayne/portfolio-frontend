109879c4be7c21f767fe4c60e0afe3ede21a3f81
Oliver Payne <olliepayne@me.com>
2024-10-07 22:38:59 -0700

    Begin scaffolding project slug page

--- app/(routes)/projects/[slug]/page.tsx
+++ app/(routes)/projects/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import { getStrapiMedia } from "@/app/helpers/getStrapiMedia"
 import { Project } from "@/app/types"
 import getStrapiData from "@/app/helpers/getStrapiData"
+import Container from "@/app/_components/Container"
+import Heading from "@/app/_components/Heading"
+import SectionNav from "@/app/_components/SectionNav"
 
 export async function generateStaticParams() {
   const projects: Project[] = await getStrapiData("/api/projects", "no-cache")
@@ -15,13 +18,24 @@ interface Props {
 }
 
 export default async function ProjectSlugPage({ params }: Props) {
-  const [project]: Project[] = await getStrapiData(
+  const [{ name }]: Project[] = await getStrapiData(
     `/api/projects?filters[slug][$eq]=${params.slug}`
   )
 
   return (
-    <main>
-      <h1>{project.name}</h1>
+    <main className="">
+      <Container variant="narrow">
+        <Heading level="h1">{name}</Heading>
+        <SectionNav />
+        <Heading level="h2">Goals</Heading>
+        <div className="[min-height:500px]" />
+        <Heading level="h2">Strategy</Heading>
+        <div className="[min-height:500px]" />
+        <Heading level="h2">Results</Heading>
+        <div className="[min-height:500px]" />
+        <Heading level="h2">Lessons</Heading>
+        <div className="[min-height:500px]" />
+      </Container>
     </main>
   )
 }
