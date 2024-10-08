8f1b863bfad3615588d4ea4a85ade0c7afc48552
Oliver Payne <olliepayne@me.com>
2024-10-07 22:39:54 -0700

    Create working SectionNav component

---
+++ app/_components/SectionNav.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import LinkButton from "@/app/_components/LinkButton"
+import TextLink from "@/app/_components/TextLink"
+import { useEffect, useState } from "react"
+
+interface NavLink {
+  sectionId: string
+  text: string
+}
+
+export default function SectionNav() {
+  const [navLinks, setNavLinks] = useState<NavLink[]>([])
+
+  useEffect(() => {
+    function generateSectionId(headingText: string) {
+      const regex = /[?.#()-]/g
+      return headingText.toLowerCase().replace(regex, "").split(" ").join("-")
+    }
+
+    const h2s = document
+      .getElementsByTagName("main")[0]
+      .getElementsByTagName("h2")
+    const newState: NavLink[] = []
+    for (let i = 0; i < h2s.length; i++) {
+      const text = h2s[i].innerText
+      const sectionId = generateSectionId(text)
+      newState.push({
+        sectionId,
+        text
+      })
+
+      // TODO we can append our "fake region" anchor here to account for the sticky navs
+      h2s[i].id = sectionId
+    }
+    setNavLinks(newState)
+  }, [])
+
+  return (
+    <nav className="sticky top-16 py-1 px-2 border-solid border-b-2 border-themeLightGray bg-white">
+      <span className="mr-4 font-bold">Contents:</span>
+      {navLinks.length > 0 && (
+        <ul className="inline-flex flex-wrap">
+          {navLinks.map(({ sectionId, text }, index) => (
+            <li
+              key={`section-nav=${index}`}
+              className={`${index < navLinks.length - 1 ? "mr-4" : ""}`}
+            >
+              <TextLink href={`#${sectionId}`} text={text} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </nav>
+  )
+}
