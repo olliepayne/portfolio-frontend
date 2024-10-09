1a1535234eb8b86d53d2f4cd36458a4190bb1589
Oliver Payne <olliepayne@me.com>
2024-10-08 21:38:38 -0700

    Redesign project card

--- app/_components/ProjectCard.tsx
+++ app/_components/ProjectCard.tsx
@@ -3,6 +3,7 @@ import { getStrapiMedia } from "@/app/helpers/getStrapiMedia"
 import LinkButton from "@/app/_components/LinkButton"
 import SkillTag from "@/app/_components/SkillTag"
 import { Project } from "@/app/types"
+import Link from "next/link"
 
 export default function ProjectCard({
   name,
@@ -12,36 +13,30 @@ export default function ProjectCard({
   skills
 }: Project) {
   return (
-    <div className="relative shadow-lg rounded-lg">
-      <div className="relative w-full h-48 shadow-lg rounded-lg">
-        <Image
-          src={getStrapiMedia(mainImage.url) as string}
-          alt={mainImage.alternativeText}
-          fill
-          className="top-0 left-0 w-full h-full rounded-lg"
-          objectFit="cover"
-        />
-      </div>
-      <div className="py-4 px-4">
-        <p className="text-heading4Desktop font-bold">{name}</p>
-        <p>{summary}</p>
-        <div className="mt-4">
-          {skills && (
-            <ul className="inline-flex flex-wrap">
-              {skills.map((skill) => (
-                <li key={`project-${name}-${skill.name}`} className="mr-2">
-                  <SkillTag href={"#"} name={skill.name} />
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-        <LinkButton
-          href={`/projects/${slug}`}
-          text="View project"
-          className="mt-4"
-        />
-      </div>
+    <div className="relative z-30 p-4 h-full group cursor-pointer bg-themeGray">
+      <Image
+        src={getStrapiMedia(mainImage.url) as string}
+        alt={mainImage.alternativeText}
+        fill
+        objectFit="cover"
+        className="-z-20"
+      />
+      <div className="absolute -z-10 top-0 left-0 w-full h-full bg-charcoal opacity-80 transition-opacity group-hover:opacity-85" />
+      <p className="text-heading3Mobile font-bold">{name}</p>
+      <p className="mt-4">{summary}</p>
+      <ul className="mt-4">
+        {skills.map((skill) => (
+          <li>
+            <SkillTag href="" name={skill.name} />
+          </li>
+        ))}
+      </ul>
+      <Link
+        href={`/projects/${slug}`}
+        className="mt-4 text-primary block opacity-0 transition-opacity before:z-20 before:absolute before:w-full before:h-full before:top-0 before:left-0 group-hover:opacity-100"
+      >
+        View project
+      </Link>
     </div>
   )
 }
