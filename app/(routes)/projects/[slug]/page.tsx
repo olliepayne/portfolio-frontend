352722e975c635d5d5c845891328c3a57a0d580e
Oliver Payne <olliepayne@me.com>
2024-10-10 11:33:06 -0700

    Refactor styles

--- app/(routes)/projects/[slug]/page.tsx
+++ app/(routes)/projects/[slug]/page.tsx
@@ -50,14 +50,14 @@ export default async function ProjectSlugPage({ params }: Props) {
             className="-z-20"
           />
           <div className="absolute -z-10 top-0 left-0 w-full h-full bg-charcoal opacity-80 transition-opacity group-hover:opacity-85" />
-          <div className="flex flex-col justify-center">
+          <div className="w-1/2">
             <Heading level="h1">{name}</Heading>
             <p>{summary}</p>
             <SkillTagsList skills={skills} className="mt-4" />
           </div>
         </Container>
       </section>
-      <section className="text-white bg-charcoal pt-32 pb-72">
+      <section className="text-white bg-charcoal pt-32 pb-[20.5rem]">
         <Container>
           <Heading level="h2">Request</Heading>
           <div className="border-l-4 border-themeLightGray border-solid pl-8 mt-4">
@@ -67,9 +67,9 @@ export default async function ProjectSlugPage({ params }: Props) {
           </div>
         </Container>
       </section>
-      <section className="pb-32 pt-72 relative">
+      <section className="pb-32 pt-[20.5rem] relative">
         <Container variant="narrow">
-          <div className="absolute w-[calc(100%-32px)] max-w-[768px] h-[384px] -top-48 left-1/2 -translate-x-1/2">
+          <div className="absolute w-[calc(100%-32px)] max-w-[768px] h-[400px] -top-48 left-1/2 -translate-x-1/2">
             <Image
               src={getStrapiMedia(secondaryImage.url) as string}
               alt={mainImage.alternativeText}
@@ -87,7 +87,7 @@ export default async function ProjectSlugPage({ params }: Props) {
       </section>
       <section className="py-32 bg-charcoal text-white">
         <Container>
-          <div className="flex flex-col justify-between items-center lg:flex-row">
+          <div className="flex flex-col justify-between lg:items-center lg:flex-row">
             <div className="basis-1/2 mr-0 lg:mr-4">
               <Heading level="h2">Results</Heading>
               <div className="border-l-4 border-primary border-solid pl-8 mt-4">
@@ -96,7 +96,7 @@ export default async function ProjectSlugPage({ params }: Props) {
                 </Markdown>
               </div>
             </div>
-            <div className="relative basis-[400px] w-full mt-16 lg:mt-0 lg:basis-1/2 lg:h-[400px]">
+            <div className="relative basis-[400px] w-full mt-16 lg:mt-0 lg:basis-1/2 lg:h-[500px]">
               <Image
                 src={getStrapiMedia(resultsImage.url) as string}
                 alt={resultsImage.alternativeText}
