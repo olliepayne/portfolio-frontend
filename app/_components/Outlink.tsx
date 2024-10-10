ed2a8af51e4242bc18c2441a699d8f041b0a1119
Oliver Payne <olliepayne@me.com>
2024-10-10 13:55:14 -0700

    Update underline height

--- app/_components/Outlink.tsx
+++ app/_components/Outlink.tsx
@@ -10,7 +10,7 @@ export default function Outlink({ children, ...props }: Props) {
   return (
     <a
       {...props}
-      className="inline-block relative z-20 hover:text-primary transition-all after:absolute after:bottom-0.5 after:left-0 after:w-full after:h-1 after:bg-primary after:-z-10"
+      className="inline-block relative z-20 hover:text-primary transition-all after:absolute after:bottom-0.5 after:left-0 after:w-full after:h-0.5 after:bg-primary after:-z-10"
     >
       {children}
     </a>
