1698593ea82a42f92498ff5bedff3671f244deb3
Oliver Payne <olliepayne@me.com>
2024-10-10 13:25:04 -0700

    Refactor

---
+++ app/_components/TextInternalLink.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react"
+import Link, { LinkProps } from "next/link"
+
+interface Props extends LinkProps {
+  children?: ReactNode | ReactNode[]
+}
+
+export default function TextInternalLink({ children, ...props }: Props) {
+  return (
+    <Link
+      {...props}
+      className="relative hover:text-primary transition-all after:absolute after:bottom-0.5 after:left-0 after:w-full after:h-1 after:bg-primary after:-z-10"
+    >
+      {children}
+    </Link>
+  )
+}
