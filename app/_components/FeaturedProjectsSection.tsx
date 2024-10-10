fd13aa72f6ae8b4d3bfc94cb67588916ec9a56c0
Oliver Payne <olliepayne@me.com>
2024-10-10 11:40:30 -0700

    Refactor skills list with component

--- app/_components/FeaturedProjectsSection.tsx
+++ app/_components/FeaturedProjectsSection.tsx
@@ -1,14 +1,23 @@
-import { Project } from "@/app/types"
 import Container from "@/app/_components/Container"
 import Heading from "@/app/_components/Heading"
 import ProjectCard from "@/app/_components/ProjectCard"
+import getStrapiData from "@/app/helpers/getStrapiData"
 
 interface Props {
-  projects: Project[]
   variant: "dark" | "light"
 }
 
-export default function FeaturedProjectsSection({ projects, variant }: Props) {
+export default async function FeaturedProjectsSection({ variant }: Props) {
+  const populateMain =
+    "populate[0]=main&populate[1]=main.mainImage&populate[2]=main.skills"
+  const populateSecond =
+    "populate[3]=second&populate[4]=second.mainImage&populate[5]=second.skills"
+  const populateThird =
+    "populate[6]=third&populate[7]=third.mainImage&populate[8]=third.skills"
+  const { main, second, third } = await getStrapiData(
+    `/api/featured-projects-section?${populateMain}&${populateSecond}&${populateThird}`
+  )
+
   return (
     <section
       className={`py-32 relative ${
@@ -22,18 +31,17 @@ export default function FeaturedProjectsSection({ projects, variant }: Props) {
       <Container>
         <Heading level="h2">Featured Projects</Heading>
         <p>What I've worked on, both personal and professional.</p>
-        {projects && (
-          <ul className="grid mt-8 gap-4 auto-rows-[250px] lg:grid-cols-2">
-            {projects.map((project: Project, index: number) => (
-              <li
-                key={`project-${index}`}
-                className={`${index === 0 ? "row-[span_2_/_auto]" : undefined}`}
-              >
-                <ProjectCard {...project} />
-              </li>
-            ))}
-          </ul>
-        )}
+        <ul className="grid mt-8 gap-8 auto-rows-[250px] lg:grid-cols-2">
+          <li className="lg:row-[span_2_/_auto]">
+            <ProjectCard {...main} />
+          </li>
+          <li>
+            <ProjectCard {...second} />
+          </li>
+          <li>
+            <ProjectCard {...third} />
+          </li>
+        </ul>
       </Container>
     </section>
   )
