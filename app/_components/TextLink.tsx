d3932a0a9765ddb143e753c5fdcfde1cb85b3df8
Oliver Payne <olliepayne@me.com>
2024-10-10 13:07:23 -0700

    Change hover color

--- app/_components/TextLink.tsx
+++ app/_components/TextLink.tsx
@@ -9,7 +9,7 @@ export default function TextLink({ href, text }: Props) {
   return (
     <Link
       href={href}
-      className="relative hover:text-gray-500 transition-all after:absolute after:bottom-0.5 after:left-0 after:w-full after:h-1 after:bg-primary after:-z-10"
+      className="relative hover:text-primary transition-all after:absolute after:bottom-0.5 after:left-0 after:w-full after:h-1 after:bg-primary after:-z-10"
     >
       {text}
     </Link>
