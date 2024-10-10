016c6ff1efaab8a8035c9f8b69eb0919d441cdec
Oliver Payne <olliepayne@me.com>
2024-10-10 13:20:23 -0700

    Refactor

--- app/_components/TextLink.tsx
+++ app/_components/TextLink.tsx
@@ -1,17 +1,17 @@
-import Link from "next/link"
+import { ReactNode } from "react"
+import Link, { LinkProps } from "next/link"
 
-interface Props {
-  href: string
-  text: string
+interface Props extends LinkProps {
+  children: ReactNode | ReactNode[]
 }
 
-export default function TextLink({ href, text }: Props) {
+export default function TextLink({ children, ...props }: Props) {
   return (
     <Link
-      href={href}
+      {...props}
       className="relative hover:text-primary transition-all after:absolute after:bottom-0.5 after:left-0 after:w-full after:h-1 after:bg-primary after:-z-10"
     >
-      {text}
+      {children}
     </Link>
   )
 }
