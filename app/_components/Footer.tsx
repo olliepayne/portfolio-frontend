e06abfc700a89ef79b5e2b7388945f8d02dddf71
Oliver Payne <olliepayne@me.com>
2024-10-10 13:55:44 -0700

    Add email and resume links to footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -3,6 +3,7 @@ import NavLink from "@/app/_components/NavLink"
 import Link from "next/link"
 import SocialIcon from "@/app/_components/SocialIcon"
 import LinkButton from "@/app/_components/LinkButton"
+import Outlink from "@/app/_components/Outlink"
 
 const linkMenus = [
   {
@@ -46,8 +47,20 @@ export default function Footer() {
           ))}
         </div>
         <div className="flex flex-col items-center mt-8 border-themeLightGray border-solid border-t-[1px] pt-8">
-          <ul className="w-fit mx-auto">
-            <li className="inline-block align-middle mr-4">
+          <ul className="flex flex-col items-center">
+            <li>
+              <Outlink href="mailto:olliepayne@me.com?subject=Portfolio%20Site%20Inquiry">
+                olliepayne@me.com
+              </Outlink>
+            </li>
+            <li className="mt-2">
+              <Outlink href="" download>
+                Download Resume
+              </Outlink>
+            </li>
+          </ul>
+          <ul className="flex justify-between items-center mt-4">
+            <li className="mr-4">
               <a
                 href="https://github.com/olliepayne"
                 target="_blank"
@@ -56,7 +69,7 @@ export default function Footer() {
                 <SocialIcon platform="GitHub" />
               </a>
             </li>
-            <li className="inline-block align-middle">
+            <li>
               <a
                 href="https://www.linkedin.com/in/oliverpayne01/"
                 target="_blank"
