3bf6c72fddda4c9ecb9bf3c29f63dcd9e79dde1d
Oliver Payne <olliepayne@me.com>
2024-10-10 12:19:32 -0700

    Finish Footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,6 +1,7 @@
 import Container from "@/app/_components/Container"
 import NavLink from "@/app/_components/NavLink"
 import Link from "next/link"
+import SocialIcon from "@/app/_components/SocialIcon"
 
 const linkMenus = [
   {
@@ -29,26 +30,41 @@ const linkMenus = [
 export default function Footer() {
   return (
     <footer className="bg-charcoal py-16 text-white">
-      <Container>
-        <div className="flex justify-between items-center">
-          <div className="basis-1/3">
-            <Link href="/" className="text-heading5Desktop font-bold">
-              Ollie Payne
-            </Link>
-            <div className="flex mt-4 justify-between">
-              {linkMenus.map((linkMenu, index) => (
-                <div
-                  key={`footer-link-menu-${index}`}
-                  // className={`${
-                  //   index < linkMenus.length - 1 ? "mr-4" : undefined
-                  // }`}
-                >
-                  <NavLink {...linkMenu.primaryLink} variant="footer menu" />
-                </div>
-              ))}
+      <Container variant="narrow">
+        <Link href="/" className="text-heading5Desktop font-bold">
+          Ollie Payne
+        </Link>
+        <div className="flex flex-wrap mt-4 justify-between">
+          {linkMenus.map((linkMenu, index) => (
+            <div
+              key={`footer-link-menu-${index}`}
+              className={`${index < linkMenus.length - 1 ? "mr-4" : undefined}`}
+            >
+              <NavLink {...linkMenu.primaryLink} variant="footer menu" />
             </div>
-          </div>
-          <div>{/* social links */}</div>
+          ))}
+        </div>
+        <div className="mt-8 border-themeLightGray border-solid border-t-2 pt-8">
+          <ul className="w-fit mx-auto">
+            <li className="inline-block align-middle mr-4">
+              <a
+                href="https://github.com/olliepayne"
+                target="_blank"
+                className="inline-block"
+              >
+                <SocialIcon platform="GitHub" />
+              </a>
+            </li>
+            <li className="inline-block align-middle">
+              <a
+                href="https://www.linkedin.com/in/oliverpayne01/"
+                target="_blank"
+                className="inline-block"
+              >
+                <SocialIcon platform="LinkedIn" />
+              </a>
+            </li>
+          </ul>
         </div>
       </Container>
     </footer>
