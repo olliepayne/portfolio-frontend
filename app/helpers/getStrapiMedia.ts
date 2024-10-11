813f41c9347d4236d37eedaf58bf33ed31e7adb0
Oliver Payne <olliepayne@me.com>
2024-10-11 00:10:24 -0700

    Test image loader fixes

--- app/helpers/getStrapiMedia.ts
+++ app/helpers/getStrapiMedia.ts
@@ -1,5 +1,10 @@
-import getStrapiUrl from "@/app/helpers/getStrapiUrl"
+// import getStrapiUrl from "@/app/helpers/getStrapiUrl"
 
 export function getStrapiMedia(url: string | null) {
-  return `${getStrapiUrl()}${url}`
+  // return `${getStrapiUrl()}${url}`
+  if (process.env.NODE_ENV === "development") {
+    return `http://localhost:1337${url}`
+  } else if (process.env.NODE_ENV === "production") {
+    return url
+  }
 }
