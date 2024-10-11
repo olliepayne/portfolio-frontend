559032e8a2968ed7a59fa18bb2de57b7023854b7
Oliver Payne <olliepayne@me.com>
2024-10-10 23:52:08 -0700

    Test fix

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "funny-oasis-8038f3a347.media.strapiapp.com"
+        hostname: "funny-oasis-8038f3a347.strapiapp.com"
       }
     ]
   }
