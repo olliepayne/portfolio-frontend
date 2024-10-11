e5d596f3a38c0fb0ad5cb4a1fe0ab58c325f27c8
Oliver Payne <olliepayne@me.com>
2024-10-10 23:22:34 -0700

    Refactor

--- app/helpers/getStrapiMedia.ts
+++ app/helpers/getStrapiMedia.ts
@@ -1,8 +1,5 @@
 import getStrapiUrl from "@/app/helpers/getStrapiUrl"
 
 export function getStrapiMedia(url: string | null) {
-  if (url == null) return null
-  if (url.startsWith("data:")) return url
-  if (url.startsWith("http") || url.startsWith("//")) return url
   return `${getStrapiUrl()}${url}`
 }
