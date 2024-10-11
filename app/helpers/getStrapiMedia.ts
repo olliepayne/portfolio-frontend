66feb6a1371061deec7bb9ae3910bd74e6f6364c
Oliver Payne <olliepayne@me.com>
2024-10-10 23:45:58 -0700

    Fix

--- app/helpers/getStrapiMedia.ts
+++ app/helpers/getStrapiMedia.ts
@@ -1,5 +1,7 @@
-import getStrapiUrl from "@/app/helpers/getStrapiUrl"
-
 export function getStrapiMedia(url: string | null) {
-  return `${getStrapiUrl()}${url}`
+  if (process.env.NODE_ENV === "development") {
+    return `http://localhost:1337${url}`
+  } else if (process.env.NODE_ENV === "production") {
+    return `${process.env.STRAPI_MEDIA_URL}${url}`
+  }
 }
