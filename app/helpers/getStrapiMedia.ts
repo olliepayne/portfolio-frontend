8ca6fa52276b6d93419a6b3157f2e4026505ca36
Oliver Payne <olliepayne@me.com>
2024-10-10 23:54:47 -0700

    Refactor

--- app/helpers/getStrapiMedia.ts
+++ app/helpers/getStrapiMedia.ts
@@ -1,7 +1,5 @@
+import getStrapiUrl from "@/app/helpers/getStrapiUrl"
+
 export function getStrapiMedia(url: string | null) {
-  if (process.env.NODE_ENV === "development") {
-    return `http://localhost:1337${url}`
-  } else if (process.env.NODE_ENV === "production") {
-    return `${process.env.STRAPI_MEDIA_URL}${url}`
-  }
+  return `${getStrapiUrl()}${url}`
 }
