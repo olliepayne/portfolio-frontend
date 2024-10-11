f7539dc36e7713aa9db6b7b1bd915bbe2eec0bfa
Oliver Payne <olliepayne@me.com>
2024-10-10 21:37:41 -0700

    Update styles

--- app/_components/InternalLink.tsx
+++ app/_components/InternalLink.tsx
@@ -9,7 +9,7 @@ export default function InternalLink({ children, ...props }: Props) {
   return (
     <Link
       {...props}
-      className="relative hover:text-primary transition-all after:absolute after:bottom-0.5 after:left-0 after:w-full after:h-1 after:bg-primary after:-z-10"
+      className="z-20 relative inline-block hover:text-primary transition-all after:absolute after:bottom-0.5 after:left-0 after:w-full after:h-0.5 after:bg-primary after:-z-10"
     >
       {children}
     </Link>
