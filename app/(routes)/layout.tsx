69ffbe204d7bee949c4205a92570b438b8cd3580
Oliver Payne <olliepayne@me.com>
2024-10-14 15:12:55 -0700

    Remove next third parties

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,7 +1,6 @@
 import "@/app/globals.css"
 import Navbar from "@/app/_components/Navbar"
 import Footer from "@/app/_components/Footer"
-import { GoogleTagManager } from "@next/third-parties/google"
 
 export default function RootLayout({
   children
