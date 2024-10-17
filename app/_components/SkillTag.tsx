a55726d47f0f51ba289881a70494912360af1ed9
Oliver Payne <olliepayne@me.com>
2024-10-16 20:17:34 -0700

    Add filter applied and not applied states

--- app/_components/SkillTag.tsx
+++ app/_components/SkillTag.tsx
@@ -1,19 +1,51 @@
+"use client"
+
 import Link from "next/link"
 import { Skill } from "@/app/types"
+import { useEffect, useState } from "react"
+import { useSearchParams } from "next/navigation"
 
 interface Props extends Skill {
-  href: string
-  name: string
+  scope: "projects" | "blog"
 }
 
-export default function SkillTag({ href, name }: Props) {
+export default function SkillTag({ name, scope }: Props) {
+  const searchParams = useSearchParams()
+
+  function getIsActiveInitialState() {
+    if (searchParams.get("[skills][name][$eq]"))
+      if (searchParams.get("[skills][name][$eq]") === name) return true
+      else return false
+  }
+  const [isActive, setIsActive] = useState(getIsActiveInitialState)
+
   const regex = /[?.#()]/g
-  const formattedName = `#${name.toLowerCase().replace(regex, "").split(" ").join("-")}`
+  const formattedName = `#${name
+    .toLowerCase()
+    .replace(regex, "")
+    .split(" ")
+    .join("-")}`
+  
+  function handleIsActive() {
+    const newState = !isActive
+    setIsActive(newState)
+  }
+  
+  function getHref() {
+    if (isActive) return `/${scope}`
+    else return `/${scope}?[skills][name][$eq]=${name}`
+  }
+  let href = getHref()
 
   return (
     <Link
       href={href}
-      className="inline-block bg-black text-white text-sm py-0.5 px-1 border-primary border-[1px] transition-colors hover:bg-primary hover:text-black"
+      onClick={handleIsActive}
+      className={`inline-block  ${
+        isActive
+          ? "bg-primary text-black text-sm py-0.5 px-1 border-primary border-[1px] transition-colors"
+          : "bg-black text-white text-sm py-0.5 px-1 border-primary border-[1px] transition-colors hover:bg-primary hover:text-black"
+      }`}
     >
       {formattedName}
     </Link>
