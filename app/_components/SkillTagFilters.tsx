3bcd986e016f49952aba36cb3245ac9b83b2c201
Oliver Payne <olliepayne@me.com>
2024-10-17 13:58:27 -0700

    Add filtering logic

---
+++ app/_components/SkillTagFilters.tsx
@@ -0,0 +1,100 @@
+"use client"
+
+import { Skill } from "@/app/types"
+import { useState, useEffect } from "react"
+import { usePathname, useSearchParams, useRouter } from "next/navigation"
+
+interface Props {
+  skills: Skill[]
+  className?: string
+}
+
+export default function SkillTagFilters({ skills, className }: Props) {
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  const regex = /[?.#()]/g
+  function formatSkillName(skillName: string) {
+    const formattedSkillName = `#${skillName
+      .toLowerCase()
+      .replace(regex, "")
+      .split(" ")
+      .join("-")}`
+
+    return formattedSkillName
+  }
+
+  interface Filters {
+    skillNames: string[]
+  }
+  function getFiltersInitialState() {
+    if (searchParams.getAll("skill").length > 0) {
+      return {
+        skillNames: searchParams.getAll("skill")
+      }
+    } else {
+      return {
+        skillNames: []
+      }
+    }
+  }
+  const [filters, setFilters] = useState<Filters>(getFiltersInitialState)
+
+  function handleChange(event: any) {
+    let newSkillNames = filters.skillNames
+
+    function updateFilters() {
+      if (filters.skillNames.includes(event.target.value)) {
+        newSkillNames.splice(filters.skillNames.indexOf(event.target.value), 1)
+      } else {
+        newSkillNames = [...newSkillNames, event.target.value]
+      }
+    }
+    updateFilters()
+
+    if (newSkillNames.length > 0) {
+      const skillNamesStr: string = newSkillNames
+        .map((item, index) => `${index > 0 ? "&" : ""}skill=${item}`)
+        .join()
+        .replace(/[,]/g, "")
+      router.push(`?${skillNamesStr}`)
+    } else {
+      router.push(pathname)
+    }
+
+    setFilters({
+      skillNames: newSkillNames
+    })
+  }
+
+  function getFilterIsActive(skillName: string) {
+    if (searchParams.getAll("skill")?.includes(skillName)) return true
+    else return false
+  }
+
+  return (
+    <form className={className || ""}>
+      {skills &&
+        skills.map((skill, index) => (
+          <label
+            key={`skill-filter-${index}`}
+            className={`relative border-primary border-[1px] py-0.5 px-1 text-sm cursor-pointer transition-colors hover:bg-primary hover:text-black ${
+              getFilterIsActive(skill.name)
+                ? "bg-primary text-black"
+                : "bg-black text-white"
+            }`}
+          >
+            <input
+              type="checkbox"
+              name={formatSkillName(skill.name)}
+              value={skill.name}
+              className="absolute top-0 left-0 w-0 h-0 opacity-0"
+              onChange={handleChange}
+            />
+            <span>{skill.name}</span>
+          </label>
+        ))}
+    </form>
+  )
+}
