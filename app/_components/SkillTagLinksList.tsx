5f09aa1a3d58138730c6b0da531eae5b49a75769
Oliver Payne <olliepayne@me.com>
2024-10-18 01:01:57 -0700

    Add text variant prop

--- app/_components/SkillTagLinksList.tsx
+++ app/_components/SkillTagLinksList.tsx
@@ -4,15 +4,16 @@ import SkillTagLink from "@/app/_components/SkillTagLink"
 interface Props {
   skills: Skill[]
   scope: "projects" | "blog"
+  textVariant: "black" | "white"
   className?: string
 }
 
-export default function SkillTagLinksList({ skills, scope, className }: Props) {
+export default function SkillTagLinksList({ skills, scope, textVariant, className }: Props) {
   return (
     <ul className={`flex flex-wrap gap-2 ${className ? className : null}`}>
       {skills.map(({ name }) => (
         <li key={`skill-tag-${name}`}>
-          <SkillTagLink name={name} href={`/${scope}?skill=${name}`} />
+          <SkillTagLink name={name} href={`/${scope}?skill=${name}`} textVariant={textVariant} />
         </li>
       ))}
     </ul>
