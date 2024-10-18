aabe76bfd6e3ae288556e7141d87cd110bfbfa3d
Oliver Payne <olliepayne@me.com>
2024-10-18 01:01:40 -0700

    Refactor active state handling

--- app/_components/SkillTagLink.tsx
+++ app/_components/SkillTagLink.tsx
@@ -2,37 +2,37 @@
 
 import Link from "next/link"
 import { Skill } from "@/app/types"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useSearchParams } from "next/navigation"
 
 interface Props extends Skill {
   href: string
+  textVariant: "black" | "white"
 }
 
-export default function SkillTagLink({ name, href }: Props) {
+export default function SkillTagLink({ name, href, textVariant }: Props) {
   const searchParams = useSearchParams()
 
-  function getIsActiveInitialState() {
-    if (searchParams.getAll("skill")?.includes(name)) return true
-    else return false
-  }
-  const [isActive, setIsActive] = useState(getIsActiveInitialState)
+  const [isActive, setIsActive] = useState<boolean>()
+  function handleActiveState() {
+    let newState = false
+    if (searchParams.getAll("skill")?.includes(name)) newState = true
 
-  const regex = /[?.#()]/g
-  const formattedName = `#${name
-    .toLowerCase()
-    .replace(regex, "")
-    .split(" ")
-    .join("-")}`
+    setIsActive(newState)
+  }
+  useEffect(handleActiveState)
 
   return (
     <Link
       href={href}
       onClick={() => setIsActive(!isActive)}
-      className={`inline-block text-xs py-0.5 px-2.5 rounded-full uppercase font-bold border-primary border-[1px] transition-colors ${
+      className={`inline-block text-xs py-0.5 px-2.5 rounded-full uppercase font-bold border-primary border-2 transition-colors ${
         isActive
           ? "bg-primary text-black"
-          : "bg-black text-white hover:bg-primary hover:text-black"
+          : `bg-transparent ${
+              (textVariant === "black" && "text-black") ||
+              (textVariant === "white" && "text-white")
+            } hover:bg-primary hover:text-black`
       }`}
     >
       {name}
