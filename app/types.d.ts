b8f40195b68ebd16e940a6014ad00f3626efa871
Oliver Payne <olliepayne@me.com>
2024-10-21 16:36:12 -0700

    Update required and optional fields to strapi models

--- app/types.d.ts
+++ app/types.d.ts
@@ -36,24 +36,25 @@ export interface Job {
 export interface Company {
   name: string
   logo: StrapiImage
-  jobs: Job[]
+  jobs?: Job[]
 }
 
 export interface Project {
   name: string
   slug: string
+  seo: SEO
   mainImage: StrapiImage
   summary: string
-  skills: Skill[]
+  skills?: Skill[]
   requestContent: string
   strategyContent: string
   resultsContent: string
 }
 
 export interface FeaturedProjectsSection {
-  main: Project
-  second: Project
-  third: Project
+  main?: Project
+  second?: Project
+  third?: Project
 }
 
 export interface BlogPost {
@@ -65,5 +66,5 @@ export interface BlogPost {
   mainImage: StrapiImage
   summary: string
   seo: SEO
-  skills: Skill[]
+  skills?: Skill[]
 }
