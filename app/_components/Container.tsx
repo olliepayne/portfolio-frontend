7367ac883b743846257aaafa1c7045fb200c9d3b
Oliver Payne <olliepayne@me.com>
2024-10-20 22:51:49 -0700

    Remove height full (obsolete)

--- app/_components/Container.tsx
+++ app/_components/Container.tsx
@@ -10,25 +10,19 @@ export default function Container({ children, variant, className }: Props) {
   switch (variant) {
     case "narrow":
       return (
-        <div
-          className={`max-w-screen-md px-4 mx-auto h-full ${className || ""}`}
-        >
+        <div className={`max-w-screen-md px-4 mx-auto ${className || ""}`}>
           {children}
         </div>
       )
     case "normal":
       return (
-        <div
-          className={`max-w-screen-xl px-4 mx-auto h-full ${className || ""}`}
-        >
+        <div className={`max-w-screen-xl px-4 mx-auto ${className || ""}`}>
           {children}
         </div>
       )
     default:
       return (
-        <div
-          className={`max-w-screen-xl px-4 mx-auto h-full ${className || ""}`}
-        >
+        <div className={`max-w-screen-xl px-4 mx-auto ${className || ""}`}>
           {children}
         </div>
       )
