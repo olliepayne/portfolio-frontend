3248c1ad0af0a27b1c67f29aa08d85363d25efdc
Oliver Payne <olliepayne@me.com>
2025-07-29 22:29:33 -0700

    Comment out unused variable

--- app/_components/ExperienceTimeline.tsx
+++ app/_components/ExperienceTimeline.tsx
@@ -74,7 +74,7 @@ export default async function ExperienceTimeline() {
         (
           {
             title,
-            employmentType,
+            // employmentType,
             startDate,
             endDate,
             location,
