c86dbb1a571bd6fb3ae44fe8c484225df669d160
Oliver Payne <olliepayne@me.com>
2025-08-08 00:21:19 -0700

    Simplify styling

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -42,7 +42,7 @@ export default async function Footer() {
   const resume: Resume = await getStrapiData("/api/resume?populate=*")
 
   return (
-    <footer className="bg-charcoal py-8 text-white">
+    <footer className="bg-charcoal pb-16 pt-4 lg:py-4 text-white">
       <Container variant="narrow">
         {/* <ul className="flex flex-wrap mt-4 justify-between">
           {linkMenus.map((linkMenu, index) => (
@@ -52,52 +52,44 @@ export default async function Footer() {
           ))}
         </ul> */}
         {/* <div className="flex flex-col justify-between items-center mt-8 gap-4 border-themeLightGray border-solid border-t-[1px] pt-8 lg:flex-row"> */}
-        <div className="flex flex-col justify-between items-center gap-4 lg:flex-row">
-          <ul>
-            <li className="mt-2">
-              <Outlink
-                href={
-                  resume?.file?.url
-                    ? (getStrapiMedia(resume.file.url) as string)
-                    : "#"
-                }
-                text="Resume"
-              />
-            </li>
-          </ul>
-          <ul className="flex justify-between items-center">
-            <li className="mr-4">
-              <a
-                href="https://www.linkedin.com/in/oliverpayne01/"
-                rel="noreferrer noopener nofollow"
-                target="_blank"
-                className="inline-block"
-              >
-                <span className="hidden">LinkedIn</span>
-                <SocialIcon platform="LinkedIn" className="fill-white" />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/olliepayne"
-                rel="noreferrer noopener nofollow"
-                target="_blank"
-                className="inline-block"
-              >
-                <span className="hidden">
-                  GitHub
-                </span>
-                <SocialIcon platform="GitHub" className="fill-white" />
-              </a>
-            </li>
-          </ul>
-          <LinkButton
-            href="/contact"
-            text="Contact Me"
-            variant="primary"
-            className="hover:text-black"
-          />
-        </div>
+        <ul className="flex justify-center items-center">
+          <li className="mr-8">
+            <Outlink
+              href={
+                resume?.file?.url
+                  ? (getStrapiMedia(resume.file.url) as string)
+                  : "#"
+              }
+              text="Resume"
+            />
+          </li>
+          <li className="mr-8">
+            <a
+              href="https://www.linkedin.com/in/oliverpayne01/"
+              rel="noreferrer noopener nofollow"
+              target="_blank"
+              className="relative"
+            >
+              <span className="pointer-events-none opacity-0 absolute">
+                LinkedIn
+              </span>
+              <SocialIcon platform="LinkedIn" className="fill-white" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/olliepayne"
+              rel="noreferrer noopener nofollow"
+              target="_blank"
+              className="relative"
+            >
+              <span className="pointer-events-none opacity-0 absolute">
+                GitHub
+              </span>
+              <SocialIcon platform="GitHub" className="fill-white" />
+            </a>
+          </li>
+        </ul>
       </Container>
     </footer>
   )
