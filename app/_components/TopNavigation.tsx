64218117c6041f8bc68a4d12856aebbe31d7ccf3
Oliver Payne <olliepayne@me.com>
2025-09-03 12:10:21 -0700

    Create

---
+++ app/_components/TopNavigation.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link"
+
+const links = [
+  {
+    href: "/#portfolio",
+    text: "Portfolio"
+  },
+  {
+    href: "/#work-history",
+    text: "Work history"
+  }
+]
+
+export default function TopNavigation() {
+  return (
+    <nav className="max-w-6xl mx-auto px-4 py-4">
+      <div className="flex justify-between">
+        <Link href="/" className="transition-colors hover:text-primary-intense">Home</Link>
+        {/* <span>Ollie Payne</span> */}
+        <ul className="flex">
+          {links.map((item, index) => (
+            <li className="ml-8" key={`top-nav-link-${index}`}>
+              <Link
+                href={item.href}
+                className="transition-colors hover:text-primary-intense"
+              >
+                {item.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  )
+}
