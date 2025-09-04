eabec839f2c0e193b9fb500163d6788a93eb9f25
Oliver Payne <olliepayne@me.com>
2025-09-04 10:30:47 -0700

    Add Container & border

--- app/_components/TopNavigationBar.tsx
+++ app/_components/TopNavigationBar.tsx
@@ -1,3 +1,4 @@
+import Container from "@/app/_components/Container"
 import Link from "next/link"
 
 const links = [
@@ -13,22 +14,26 @@ const links = [
 
 export default function TopNavigationBar() {
   return (
-    <nav className="max-w-6xl mx-auto px-4 py-4">
-      <div className="flex justify-between">
-        <Link href="/" className="transition-colors hover:text-primary">Home</Link>
-        <ul className="flex">
-          {links.map((item, index) => (
-            <li className="ml-8" key={`top-nav-link-${index}`}>
-              <Link
-                href={item.href}
-                className="transition-colors hover:text-primary"
-              >
-                {item.text}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+    <nav className="py-4 border-b-[1px] border-light-gray">
+      <Container>
+        <div className="flex justify-between">
+          <Link href="/" className="transition-colors hover:text-primary">
+            Home
+          </Link>
+          <ul className="flex">
+            {links.map((item, index) => (
+              <li className="ml-8" key={`top-nav-link-${index}`}>
+                <Link
+                  href={item.href}
+                  className="transition-colors hover:text-primary"
+                >
+                  {item.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Container>
     </nav>
   )
 }
