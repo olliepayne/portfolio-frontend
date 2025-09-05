4297f29b5cf87ec55b14e3c746298be28bb13328
Oliver Payne <olliepayne@me.com>
2025-09-04 22:01:37 -0700

    Add key prop

--- app/_components/ProjectsGrid.tsx
+++ app/_components/ProjectsGrid.tsx
@@ -11,6 +11,7 @@ export default async function ProjectsGrid() {
     <div className="grid gap-8 grid-cols-1 md:grid-rows-[400px] auto-rows-[300px] md:grid-cols-2">
       {data.map((item, index) => (
         <ProjectCard
+        key={index}
           {...item}
           className={cn(index === 0 && "md:col-span-2")}
         />
