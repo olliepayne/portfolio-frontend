aa29c6268489bea2c520f1f0fff1efc315051a0b
Oliver Payne <olliepayne@me.com>
2025-09-04 22:02:26 -0700

    Update shadow and update anchor to Next.js Link

--- app/_components/ProjectCard.tsx
+++ app/_components/ProjectCard.tsx
@@ -19,7 +19,7 @@ export default function ProjectCard({
   const mergedStyles = cn(baseStyles, className)
   return (
     <div className={mergedStyles}>
-      <div className="relative flex-1 rounded-sm shadow-md">
+      <div className="relative flex-1 rounded-sm shadow-sm">
         <Image
           src={getStrapiMedia(mainImage.url) as string}
           alt={mainImage.alternativeText}
@@ -28,13 +28,15 @@ export default function ProjectCard({
         />
         <div className="absolute rounded-sm left-0 top-0 w-full h-full group-hover:border-4 transition-all dark:border-primary-intense border-primary"></div>
       </div>
-      <p className="mt-2 w-fit relative after:-z-10 after:absolute after:left-0 after:bottom-0.5 after:w-full after:h-[2px] after:opacity-0 dark:after:bg-off-white after:bg-off-black group-hover:after:opacity-100 after:transition-all">{name}</p>
-      <a
+      <p className="mt-2 w-fit relative after:-z-10 after:absolute after:left-0 after:bottom-0.5 after:w-full after:h-[2px] after:opacity-0 dark:after:bg-off-white after:bg-off-black group-hover:after:opacity-100 after:transition-all">
+        {name}
+      </p>
+      <Link
         href={`/projects/${slug}`}
         className="absolute top-0 left-0 w-full h-full inline-block opacity-0"
       >
         View project
-      </a>
+      </Link>
     </div>
   )
 }
