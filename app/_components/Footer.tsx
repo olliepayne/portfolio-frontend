b12a74ac8c00fc756f61c8b3c4c9a40a3a5fed90
Oliver Payne <olliepayne@me.com>
2025-09-05 00:32:04 -0700

    Add ThemeToggleButton

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,10 +1,16 @@
 import Container from "@/app/_components/Container"
+import ThemeToggleButton from "@/app/_components/ThemeToggleButton"
 
 export default function Footer() {
   return (
-    <footer className="py-8 mt-8">
+    <footer className="py-16">
       <Container>
-        <span className="dark:text-off-white text-body-sm text-gray">Copyright &copy; {new Date().getFullYear()}, Oliver Payne</span>
+        <div className="flex justify-between items-center">
+          <span className="dark:text-off-white text-body-sm text-gray">
+            Copyright &copy; {new Date().getFullYear()}, Oliver Payne
+          </span>
+          <ThemeToggleButton />
+        </div>
       </Container>
     </footer>
   )
