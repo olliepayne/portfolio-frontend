f3f1f2a6c18c0716201e41670b97c68cad928b9b
Oliver Payne <olliepayne@me.com>
2025-09-05 14:30:35 -0700

    Add hotfix live test

--- app/_components/ThemeSwitch.tsx
+++ app/_components/ThemeSwitch.tsx
@@ -13,11 +13,13 @@ export default function ThemeSwitch({ className }: ThemeSwitchProps) {
 
   useEffect(() => {
     setMounted(true)
-    
-    if (window.matchMedia("(prefers-color-scheme: dark)")) {
-      setTheme("dark")
-    } else {
-      setTheme("light")
+
+    if (theme === undefined) {
+      if (window.matchMedia("(prefers-color-scheme: dark)")) {
+        setTheme("dark")
+      } else {
+        setTheme("light")
+      }
     }
   }, [])
 
