f494366ab64ae2f619cb94a1f49754fe7bb58ea8
Oliver Payne <olliepayne@me.com>
2025-09-05 09:36:12 -0700

    Add state

--- app/_components/ThemeToggleButton.tsx
+++ app/_components/ThemeToggleButton.tsx
@@ -12,13 +12,21 @@ export default function ThemeToggleButton({
 }: ThemeToggleButtonProps) {
   const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
+  const [themeState, setThemeState] = useState("")
 
   useEffect(() => {
     setMounted(true)
+    setThemeState(theme as string)
   }, [])
 
   if (!mounted) {
     return null
+  }
+
+  function handleClick() {
+    const otherTheme = theme === "dark" ? "light" : "dark"
+    setTheme(otherTheme)
+    setThemeState(otherTheme)
   }
 
   return (
@@ -27,19 +35,19 @@ export default function ThemeToggleButton({
         className,
         "group dark:hover:text-off-white hover:text-off-black hover:bg-gray-200 dark:hover:bg-gray-600 dark:bg-gray-700 transition-all inline-block rounded-full w-[54px] h-[28px] bg-gray-100 relative text-transparent cursor-pointer"
       )}
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      onClick={handleClick}
     >
       {/* Slider inside */}
       <span
         className={cn(
           "bottom-1/2 translate-y-1/2 w-[20px] aspect-square bg-gray-300 absolute dark:group-hover:bg-gray-200 group-hover:bg-gray-400 rounded-full transition-all",
-          theme === "dark" ? "left-7" : "left-1"
+          themeState === "dark" ? "left-7" : "left-1"
         )}
       ></span>
 
       {/* Text */}
       <span className="absolute w-fit -left-full -translate-x-1/2 bottom-1/2 translate-y-1/2 text-body-sm">
-        Enable {theme === "dark" ? "light" : "dark"} mode
+        Enable {themeState === "dark" ? "light" : "dark"} mode
       </span>
     </button>
   )
