43cdb73e3f92bd45e149bf77a2fbeda34618fd3e
Oliver Payne <olliepayne@me.com>
2025-09-04 21:14:41 -0700

    Fix

--- app/_components/ThemeToggleButton.tsx
+++ app/_components/ThemeToggleButton.tsx
@@ -1,6 +1,7 @@
 "use client"
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { cn } from "@/app/_utils/cn"
+import { setTheme, getTheme } from "@/app/_utils/themeHandlers"
 
 type Theme = "dark" | "light"
 
@@ -11,52 +12,34 @@ type ThemeToggleButtonProps = {
 export default function ThemeToggleButton({
   className
 }: ThemeToggleButtonProps) {
-  const [isDark, setIsDark] = useState<boolean>()
-
-  function setTheme(theme: Theme) {
-    if (theme == "dark") {
-      document.body.classList.add("dark-mode")
-      setIsDark(true)
-    } else {
-      document.body.classList.remove("dark-mode")
-      setIsDark(false)
-    }
-
-    localStorage.setItem("theme", theme)
-  }
+  const [isDark, setIsDark] = useState<boolean>(
+    getTheme() === "dark" ? true : false
+  )
 
   function handleClick() {
-    if (localStorage.getItem("theme") === "dark") {
-      setTheme("light")
-      setIsDark(false)
-    } else {
-      setTheme("dark")
+    setTheme()
+
+    if (!document.body.classList.contains("theme-transition")) {
+      document.body.classList.add("theme-transition")
+    }
+
+    if (getTheme() === "dark") {
       setIsDark(true)
+    } else {
+      setIsDark(false)
     }
   }
-
-  useEffect(() => {
-    if (localStorage.getItem("theme") === null) {
-      if (window.matchMedia("(prefers-color-scheme: dark)")) {
-        setTheme("dark")
-      } else {
-        setTheme("light")
-      }
-    } else {
-      setTheme(localStorage.getItem("theme") as Theme)
-    }
-  }, [])
 
   return (
     <button
       className={cn(
-        "group inline-block rounded-full w-[52px] h-[26px] dark:bg-off-black bg-light-gray relative after:absolute text-transparent after:bottom-1 after:w-[18px] after:aspect-square dark:after:bg-gray after:bg-off-white after:rounded-full after:transition-all",
+        "group inline-block rounded-full w-[52px] h-[26px] dark:bg-off-black bg-light-gray relative after:absolute text-transparent after:bottom-1 after:w-[18px] after:aspect-square dark:after:bg-gray after:bg-gray-50 after:rounded-full after:transition-all",
         className,
-        isDark ? "after:left-7" : "after:left-1"
+        isDark ? "after:left-7" : "after:left-1.5"
       )}
       onClick={handleClick}
     >
-      Change theme
+      Enable {isDark ? "light" : "dark"} mode
     </button>
   )
 }
