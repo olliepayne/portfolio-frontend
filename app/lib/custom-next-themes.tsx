63b5b0c01424bc5fdc99977bad0e90af9b37dc4a
Oliver Payne <olliepayne@me.com>
2025-09-07 19:55:02 -0700

    Scaffold ThemeProvider component

--- app/lib/custom-next-themes.tsx
+++ app/lib/custom-next-themes.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState, useEffect, createContext } from "react"
+import { useState, useEffect, createContext, Dispatch, SetStateAction } from "react"
 
 const systemTheme = () => {
   if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
@@ -7,4 +7,32 @@ const systemTheme = () => {
   } else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
     return "light"
   }
+}
+
+type ThemeProviderProps = {
+  childen?: React.ReactNode | React.ReactNode[]
+}
+
+const ThemeContext = createContext<[string, Dispatch<SetStateAction<string>>]>([
+  "",
+  () => {}
+])
+
+export default function ThemeProvider({ childen }: ThemeProviderProps) {
+  const [theme, setTheme] = useState("")
+
+  function loadTheme() {
+    const savedTheme = localStorage.getItem("theme") || undefined
+    if (savedTheme !== undefined) {
+      
+    } else {
+      
+    }
+  } 
+
+  return (
+    <ThemeContext.Provider value={[theme, setTheme]}>
+      {childen}
+    </ThemeContext.Provider>
+  )
 }
