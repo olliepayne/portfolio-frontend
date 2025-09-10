d4e9e4a6a50dce4e0536f9c14e1c0c9523f86cf4
Oliver Payne <olliepayne@me.com>
2025-09-09 23:09:02 -0700

    Create

---
+++ app/_components/ThemeWrappedSyntaxHighlighter.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import {
+  atomDark,
+  oneLight
+} from "react-syntax-highlighter/dist/esm/styles/prism"
+
+import { useTheme } from "@/app/_lib/theme-handlers"
+
+type ThemeWrapperProps = {
+  language: string
+  code: string
+}
+
+export default function ThemeWrappedSyntaxHighlighter({
+  language,
+  code
+}: ThemeWrapperProps) {
+  const { theme, setTheme } = useTheme()
+
+  return (
+    <div className="mt-8 shadow-sm rounded-sm text-body-sm">
+      <SyntaxHighlighter
+        style={theme === "dark" ? atomDark : oneLight}
+        PreTag="div"
+        language={language}
+      >
+        {code}
+      </SyntaxHighlighter>
+    </div>
+  )
+}
