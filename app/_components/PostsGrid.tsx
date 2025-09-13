28fb5fccfd26525227fd0a9b4bf694e07267c055
Oliver Payne <olliepayne@me.com>
2025-09-13 15:21:18 -0700

    Update styles

--- app/_components/PostsGrid.tsx
+++ app/_components/PostsGrid.tsx
@@ -1,14 +1,6 @@
 import { Post } from "@/app/types"
 import InternalLink from "@/app/_components/InternalLink"
-
-function getFormattedDateStr(dateStr: string) {
-  const formatter = Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "short"
-  })
-
-  return formatter.format(new Date(dateStr)).toString()
-}
+import { getShortMonthYear } from "@/app/_utils/dateFormatter"
 
 type PostsTableProps = {
   posts: Post[]
@@ -20,16 +12,16 @@ export default function PostsGrid({ posts }: PostsTableProps) {
       {posts.map((post, index) => (
         <div key={`post-${index}`}>
           <InternalLink href={`/posts/${post.slug}`} text={post.title} />
-          <p className="my-4">{post.summary}</p>
-          <div>
-            <span className="mr-4">
-              {getFormattedDateStr(post.publishedAt)}
-            </span>
-            {post.postCategory && (
+          {post.postCategory && (
+            <div className="my-4">
               <span className="px-2 py-1 text-sm uppercase rounded-sm border-[1px] border-off-white bg-[#FAFAFA] dark:bg-[#1D1F21] dark:border-off-black">
                 {post.postCategory.name}
               </span>
-            )}
+            </div>
+          )}
+          <p className="my-4">{post.summary}</p>
+          <div>
+            <span className="mr-4">{getShortMonthYear(post.updatedAt)}</span>
           </div>
         </div>
       ))}
