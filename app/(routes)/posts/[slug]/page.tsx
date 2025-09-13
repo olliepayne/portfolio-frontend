1a5630a3d4db31a529be18153d8daf6c9d9bb7d2
Oliver Payne <olliepayne@me.com>
2025-09-13 14:38:12 -0700

    Add content and continue styling

--- app/(routes)/posts/[slug]/page.tsx
+++ app/(routes)/posts/[slug]/page.tsx
@@ -3,6 +3,7 @@ import getStrapiData from "@/app/_utils/getStrapiData"
 import { Metadata } from "next"
 import Container from "@/app/_components/Container"
 import Heading from "@/app/_components/Heading"
+import { getShortMonthDayYear } from "@/app/_utils/dateFormatter"
 import Image from "next/image"
 import { getStrapiMedia } from "@/app/_utils/getStrapiMedia"
 import Markdown from "react-markdown"
@@ -38,7 +39,15 @@ export default async function PostPage({ params }: PostPageProps) {
   const posts: Post[] = await getStrapiData(
     `/api/posts?filters[slug][$eq]=${params.slug}&populate=*`
   )
-  const { title, mainImage } = posts[0]
+  const {
+    title,
+    mainImage,
+    summary,
+    content,
+    updatedAt,
+    publishedAt,
+    postCategory
+  } = posts[0]
 
   return (
     <main>
@@ -46,10 +55,17 @@ export default async function PostPage({ params }: PostPageProps) {
         <header className="mt-32">
           <Container>
             <Heading level="h1">{title}</Heading>
-            <div className="mt-8 flex justify-between flex-col-reverse md:flex-row">
-              <p className="mt-8 basis-2/3 text-body-large md:mt-0"></p>
-              <div className="md:ml-8">
-                <span className="font-semibold"></span>
+            <div className="mt-8 flex justify-between flex-col md:flex-row">
+              <p className="basis-2/3 text-body-large">{summary}</p>
+              <div className="mt-8 md:mt-0 md:ml-8">
+                <span className="mr-4">
+                  Updated {getShortMonthDayYear(updatedAt)}
+                </span>
+                {postCategory && (
+                  <span className="px-2 py-1 text-sm uppercase rounded-sm border-[1px] border-off-white bg-[#FAFAFA] dark:bg-[#1D1F21] dark:border-off-black">
+                    {postCategory.name}
+                  </span>
+                )}
               </div>
             </div>
             <div className="mt-16 relative w-full aspect-video rounded-sm shadow-md">
@@ -69,7 +85,9 @@ export default async function PostPage({ params }: PostPageProps) {
             <Markdown
               components={markdownComponents}
               remarkPlugins={[remarkGfm]}
-            ></Markdown>
+            >
+              {content}
+            </Markdown>
           </Container>
         </section>
       </article>
