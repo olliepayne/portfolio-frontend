38a7decdeaeadd031460b0702200312b941cef2b
Oliver Payne <olliepayne@me.com>
2025-09-13 23:19:40 -0700

    Fix row sizing

--- app/_components/ProjectsGrid.tsx
+++ app/_components/ProjectsGrid.tsx
@@ -1,19 +1,18 @@
 import { Project } from "@/app/types"
 import getStrapiData from "@/app/_utils/getStrapiData"
 import ProjectCard from "@/app/_components/ProjectCard"
-import { cn } from "@/app/_utils/cn"
 
 export default async function ProjectsGrid() {
   const url = `/api/projects?populate=*`
   const data: Project[] = await getStrapiData(url)
 
   return (
-    <div className="grid gap-8 grid-cols-1 md:grid-rows-[400px] auto-rows-[300px] md:grid-cols-2">
+    <div className="grid gap-8 grid-cols-2 grid-rows-[500px] auto-rows-[350px]">
       {data.map((item, index) => (
         <ProjectCard
-        key={index}
+          key={index}
           {...item}
-          className={cn(index === 0 && "md:col-span-2")}
+          className={index === 0 ? "md:col-span-2" : ""}
         />
       ))}
     </div>
