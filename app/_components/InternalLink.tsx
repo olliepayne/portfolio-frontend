3197f93472b38b71bcdd2c1ba1ef46a551449a07
Oliver Payne <olliepayne@me.com>
2025-09-13 22:11:07 -0700

    Add cn

--- app/_components/InternalLink.tsx
+++ app/_components/InternalLink.tsx
@@ -1,14 +1,19 @@
 import Link, { LinkProps } from "next/link"
+import { cn } from "@/app/_utils/cn"
 
 interface Props extends LinkProps {
   text: string
+  className?: string
 }
 
-export default function InternalLink({ text, ...props }: Props) {
+export default function InternalLink({ text, className, ...props }: Props) {
   return (
     <Link
       {...props}
-      className="inline-block relative dark:hover:after:bg-off-white hover:after:bg-off-black after:transition-all after:absolute after:bottom-0.5 after:left-0 after:w-full after:h-[2px] after:bg-gray-400"
+      className={cn(
+        "inline-block relative dark:hover:after:bg-off-white hover:after:bg-off-black after:transition-all after:absolute after:bottom-0.5 after:left-0 after:w-full after:h-[2px] after:bg-gray-400",
+        className
+      )}
     >
       {text}
     </Link>
