cde712f426724bed7b6924dbd8cf3defb3e05444
Oliver Payne <olliepayne@me.com>
2025-09-14 17:58:12 -0700

    Decrease padding and update cxontainer variant

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -3,8 +3,8 @@ import ThemeSwitch from "@/app/_components/ThemeSwitch"
 
 export default function Footer() {
   return (
-    <footer className="mt-32 py-16 border-t-2 dark:border-off-black border-light-gray">
-      <Container>
+    <footer className="mt-32 py-8 border-t-2 dark:border-off-black border-light-gray">
+      <Container variant="full">
         <div className="flex justify-between items-center">
           <span className="dark:text-off-white text-body-sm text-gray">
             Copyright &copy; {new Date().getFullYear()}, Oliver Payne
